773616879db6283fdab7b9698d7cf8118c9a94b3
Egorxd <95929561+Egorxd@users.noreply.github.com>
2025-10-22 21:21:59 +0300

    lab 4

--- lab4/main.c
+++ lab4/main.c
@@ -6,153 +6,168 @@
 #include <unistd.h>
 #include <ctype.h>
 
-int is_numeric_mode(const char *mode) {
-    for (int i = 0; mode[i] != '\0'; i++) {
-        if (mode[i] < '0' || mode[i] > '7') {
+int check_if_numeric(const char *input_str) {
+    int index = 0;
+    while (input_str[index] != '\0') {
+        if (input_str[index] < '0' || input_str[index] > '7') {
             return 0;
         }
+        index++;
     }
     return 1;
 }
 
-mode_t parse_numeric_mode(const char *mode) {
-    char *endptr;
-    long octal_value = strtol(mode, &endptr, 8);
+mode_t convert_octal_mode(const char *mode_string) {
+    char *terminator;
+    long octal_result = strtol(mode_string, &terminator, 8);
     
-    // Валидация
-    if (*endptr != '\0' || octal_value < 0 || octal_value > 07777) {
-        fprintf(stderr, "Ошибка: недопустимый режим '%s'\n", mode);
+    // Проверка корректности
+    if (*terminator != '\0' || octal_result < 0 || octal_result > 07777) {
+        fprintf(stderr, "Некорректный режим доступа: '%s'\n", mode_string);
         return (mode_t)-1;
     }
     
-    return (mode_t)octal_value;
+    return (mode_t)octal_result;
 }
 
-int parse_single_symbolic_mode(const char *mode, mode_t current_mode, mode_t *new_mode) {
-    const char *ptr = mode;
-    mode_t who_mask = 0;
-    char operation = 0;
-    mode_t permissions = 0;
+int process_symbolic_permission(const char *perm_string, mode_t existing_mode, mode_t *updated_mode) {
+    const char *current_pos = perm_string;
+    mode_t target_mask = 0;
+    char operation_type = 0;
+    mode_t perm_bits = 0;
     
-    int who_specified = 0;
-    while (*ptr && (*ptr == 'u' || *ptr == 'g' || *ptr == 'o' || *ptr == 'a')) {
-        who_specified = 1;
-        if (*ptr == 'u') {
-            who_mask |= S_IRWXU;
-        } else if (*ptr == 'g') {
-            who_mask |= S_IRWXG;
-        } else if (*ptr == 'o') {
-            who_mask |= S_IRWXO;
-        } else if (*ptr == 'a') {
-            who_mask |= S_IRWXU | S_IRWXG | S_IRWXO;
+    int flags_specified = 0;
+    while (*current_pos && (*current_pos == 'u' || *current_pos == 'g' || *current_pos == 'o' || *current_pos == 'a')) {
+        flags_specified = 1;
+        switch (*current_pos) {
+            case 'u':
+                target_mask |= S_IRWXU;
+                break;
+            case 'g':
+                target_mask |= S_IRWXG;
+                break;
+            case 'o':
+                target_mask |= S_IRWXO;
+                break;
+            case 'a':
+                target_mask |= S_IRWXU | S_IRWXG | S_IRWXO;
+                break;
         }
-        ptr++;
+        current_pos++;
     }
     
-    if (!who_specified) {
-        who_mask = S_IRWXU | S_IRWXG | S_IRWXO;
+    if (!flags_specified) {
+        target_mask = S_IRWXU | S_IRWXG | S_IRWXO;
     }
     
-    if (*ptr == '+' || *ptr == '-' || *ptr == '=') {
-        operation = *ptr;
-        ptr++;
+    if (*current_pos == '+' || *current_pos == '-' || *current_pos == '=') {
+        operation_type = *current_pos;
+        current_pos++;
     } else {
-        fprintf(stderr, "Ошибка: неверный формат режима '%s'\n", mode);
+        fprintf(stderr, "Неправильный синтаксис в '%s'\n", perm_string);
         return -1;
     }
     
-    while (*ptr && (*ptr == 'r' || *ptr == 'w' || *ptr == 'x')) {
-        if (*ptr == 'r') {
-            permissions |= S_IRUSR | S_IRGRP | S_IROTH; 
-        } else if (*ptr == 'w') {
-            permissions |= S_IWUSR | S_IWGRP | S_IWOTH; 
-        } else if (*ptr == 'x') {
-            permissions |= S_IXUSR | S_IXGRP | S_IXOTH; 
+    while (*current_pos && (*current_pos == 'r' || *current_pos == 'w' || *current_pos == 'x')) {
+        switch (*current_pos) {
+            case 'r':
+                perm_bits |= (S_IRUSR | S_IRGRP | S_IROTH);
+                break;
+            case 'w':
+                perm_bits |= (S_IWUSR | S_IWGRP | S_IWOTH);
+                break;
+            case 'x':
+                perm_bits |= (S_IXUSR | S_IXGRP | S_IXOTH);
+                break;
         }
-        ptr++;
+        current_pos++;
     }
     
-    if (*ptr != '\0') {
-        fprintf(stderr, "Ошибка: неверный формат режима '%s'\n", mode);
+    if (*current_pos != '\0') {
+        fprintf(stderr, "Синтаксическая ошибка в '%s'\n", perm_string);
         return -1;
     }
     
-    if (operation == '+') {
-        *new_mode = current_mode | (permissions & who_mask);
-    } else if (operation == '-') {
-        *new_mode = current_mode & ~(permissions & who_mask);
-    } else if (operation == '=') {
-        *new_mode = (current_mode & ~who_mask) | (permissions & who_mask);
+    switch (operation_type) {
+        case '+':
+            *updated_mode = existing_mode | (perm_bits & target_mask);
+            break;
+        case '-':
+            *updated_mode = existing_mode & ~(perm_bits & target_mask);
+            break;
+        case '=':
+            *updated_mode = (existing_mode & ~target_mask) | (perm_bits & target_mask);
+            break;
     }
     
     return 0;
 }
 
-int parse_symbolic_mode(const char *mode, mode_t current_mode, mode_t *new_mode) {
-    char mode_copy[256];
-    strncpy(mode_copy, mode, sizeof(mode_copy) - 1);
-    mode_copy[sizeof(mode_copy) - 1] = '\0';
+int apply_symbolic_changes(const char *mode_expression, mode_t current_perms, mode_t *result_perms) {
+    char expression_copy[512];
+    strncpy(expression_copy, mode_expression, sizeof(expression_copy) - 1);
+    expression_copy[sizeof(expression_copy) - 1] = '\0';
     
-    *new_mode = current_mode;
+    *result_perms = current_perms;
     
-    char *token = strtok(mode_copy, ",");
-    while (token != NULL) {
-        if (parse_single_symbolic_mode(token, *new_mode, new_mode) == -1) {
+    char *tokenized_part = strtok(expression_copy, ",");
+    while (tokenized_part != NULL) {
+        if (process_symbolic_permission(tokenized_part, *result_perms, result_perms) == -1) {
             return -1;
         }
-        token = strtok(NULL, ",");
+        tokenized_part = strtok(NULL, ",");
     }
     
     return 0;
 }
 
-int change_mode(const char *filename, const char *mode_str) {
-    struct stat st;
-    mode_t new_mode;
+int modify_file_permissions(const char *file_path, const char *mode_specification) {
+    struct stat file_info;
+    mode_t final_mode;
     
-    if (stat(filename, &st) == -1) {
-        perror("stat");
+    if (stat(file_path, &file_info) == -1) {
+        perror("Ошибка получения информации о файле");
         return -1;
     }
     
-    if (is_numeric_mode(mode_str)) {
-        new_mode = parse_numeric_mode(mode_str);
-        if (new_mode == (mode_t)-1) {  
+    if (check_if_numeric(mode_specification)) {
+        final_mode = convert_octal_mode(mode_specification);
+        if (final_mode == (mode_t)-1) {
             return -1;
         }
     } else {
-        if (parse_symbolic_mode(mode_str, st.st_mode, &new_mode) == -1) {
+        if (apply_symbolic_changes(mode_specification, file_info.st_mode, &final_mode) == -1) {
             return -1;
         }
     }
     
-    if (chmod(filename, new_mode) == -1) {
-        perror("chmod");
+    if (chmod(file_path, final_mode) == -1) {
+        perror("Ошибка изменения прав");
         return -1;
     }
     
-    printf("Права доступа к файлу '%s' успешно изменены\n", filename);
+    printf("Права доступа для '%s' обновлены успешно\n", file_path);
     return 0;
 }
 
-int main(int argc, char *argv[]) {
-    if (argc != 3) {
-        fprintf(stderr, "Использование: %s <режим> <файл>\n", argv[0]);
-        fprintf(stderr, "Примеры:\n");
-        fprintf(stderr, "  %s +x file.txt\n", argv[0]);
-        fprintf(stderr, "  %s u-r file.txt\n", argv[0]);
-        fprintf(stderr, "  %s g+rw file.txt\n", argv[0]);
-        fprintf(stderr, "  %s ug+rw file.txt\n", argv[0]);
-        fprintf(stderr, "  %s uga+rwx file.txt\n", argv[0]);
-        fprintf(stderr, "  %s u=rw,g=r,o= file.txt\n", argv[0]);
-        fprintf(stderr, "  %s 766 file.txt\n", argv[0]);
+int main(int arg_count, char *arg_values[]) {
+    if (arg_count != 3) {
+        fprintf(stderr, "Формат команды: %s <режим> <файл>\n", arg_values[0]);
+        fprintf(stderr, "Доступные варианты:\n");
+        fprintf(stderr, "  %s +x filename\n", arg_values[0]);
+        fprintf(stderr, "  %s u-w filename\n", arg_values[0]);
+        fprintf(stderr, "  %s g+rx filename\n", arg_values[0]);
+        fprintf(stderr, "  %s ug+rw filename\n", arg_values[0]);
+        fprintf(stderr, "  %s a+rwx filename\n", arg_values[0]);
+        fprintf(stderr, "  %s u=rwx,g=rx,o= filename\n", arg_values[0]);
+        fprintf(stderr, "  %s 755 filename\n", arg_values[0]);
         return EXIT_FAILURE;
     }
     
-    const char *mode = argv[1];
-    const char *filename = argv[2];
+    const char *permission_mode = arg_values[1];
+    const char *target_file = arg_values[2];
     
-    if (change_mode(filename, mode) == -1) {
+    if (modify_file_permissions(target_file, permission_mode) == -1) {
         return EXIT_FAILURE;
     }
     
