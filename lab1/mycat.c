50f3619ed124d94f0059ec0c08381a982386fef3
Egorxd <95929561+Egorxd@users.noreply.github.com>
2025-12-18 00:49:39 +0300

    edit mycat.c

--- lab1/mycat.c
+++ lab1/mycat.c
@@ -1,122 +1,122 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <errno.h>
 
 typedef struct {
-    int number_all_lines;
-    int number_nonblank;
-    int show_ends;
-} CatOptions;
+    int num_all;
+    int num_nonempty;
+    int mark_eol;
+} Flags;
 
-static void print_usage(const char *progname) {
-    fprintf(stderr, "Usage: %s [-n] [-b] [-E] [FILE ...]\n", progname);
-    fprintf(stderr, "       %s [--] [FILE ...]\n", progname);
+static void usage(const char *app) {
+    fprintf(stderr,
+        "Usage: %s [-n] [-b] [-E] [FILE ...]\n"
+        "       %s [--] [FILE ...]\n", app, app);
 }
 
-static int process_stream(FILE *fp, const char *name, const CatOptions *opts) {
-    int c;
-    int exit_code = 0;
-    long long line_no = 1;
-    int at_line_start = 1;
+static int copy_file(FILE *stream, const char *label, const Flags *flg) {
+    int ch;
+    int new_line = 1;
+    long long counter = 1;
+    int status = 0;
 
-    while ((c = fgetc(fp)) != EOF) {
-        if (at_line_start) {
-            int should_number = 0;
-            if (opts->number_nonblank) {
-                should_number = (c != '\n');
-            } else if (opts->number_all_lines) {
-                should_number = 1;
+    while ((ch = getc(stream)) != EOF) {
+        if (new_line) {
+            int print_num = 0;
+
+            if (flg->num_nonempty && ch != '\n') {
+                print_num = 1;
+            } else if (flg->num_all && !flg->num_nonempty) {
+                print_num = 1;
             }
-            if (should_number) {
-                fprintf(stdout, "%6lld\t", line_no);
-                line_no++;
+
+            if (print_num) {
+                printf("%6lld\t", counter++);
             }
-            at_line_start = 0;
+            new_line = 0;
         }
 
-        if (opts->show_ends && c == '\n') {
-            fputc('$', stdout);
+        if (flg->mark_eol && ch == '\n') {
+            putchar('$');
         }
 
-        fputc(c, stdout);
+        putchar(ch);
 
-        if (c == '\n') {
-            at_line_start = 1;
+        if (ch == '\n') {
+            new_line = 1;
         }
     }
 
-    if (ferror(fp)) {
-        fprintf(stderr, "mycat: error reading '%s': %s\n", name, strerror(errno));
-        exit_code = 1;
+    if (ferror(stream)) {
+        fprintf(stderr, "mycat: read error '%s': %s\n",
+                label, strerror(errno));
+        status = 1;
     }
 
-    return exit_code;
+    return status;
 }
 
-int main(int argc, char **argv) {
-    CatOptions opts;
-    memset(&opts, 0, sizeof(opts));
+int main(int argc, char *argv[]) {
+    Flags flg = {0, 0, 0};
+    int pos = 1;
+    int stop_opts = 0;
 
-    int i = 1;
-    int end_of_options = 0;
-    for (; i < argc; ++i) {
-        const char *arg = argv[i];
-        if (!end_of_options && strcmp(arg, "--") == 0) {
-            end_of_options = 1;
+    while (pos < argc) {
+        char *arg = argv[pos];
+
+        if (!stop_opts && strcmp(arg, "--") == 0) {
+            stop_opts = 1;
+            pos++;
             continue;
         }
-        if (!end_of_options && arg[0] == '-' && arg[1] != '\0') {
-            for (int j = 1; arg[j] != '\0'; ++j) {
-                char f = arg[j];
-                if (f == 'n') {
-                    opts.number_all_lines = 1;
-                } else if (f == 'b') {
-                    opts.number_nonblank = 1;
-                } else if (f == 'E') {
-                    opts.show_ends = 1;
-                } else {
-                    fprintf(stderr, "mycat: unknown option -- %c\n", f);
-                    print_usage(argv[0]);
-                    return 2;
+
+        if (!stop_opts && arg[0] == '-' && arg[1]) {
+            for (int i = 1; arg[i]; i++) {
+                switch (arg[i]) {
+                    case 'n': flg.num_all = 1; break;
+                    case 'b': flg.num_nonempty = 1; break;
+                    case 'E': flg.mark_eol = 1; break;
+                    default:
+                        fprintf(stderr, "mycat: invalid option -- %c\n", arg[i]);
+                        usage(argv[0]);
+                        return 2;
                 }
             }
+            pos++;
         } else {
             break;
         }
     }
 
-    if (opts.number_nonblank) {
-        opts.number_all_lines = 0;
+    if (flg.num_nonempty) {
+        flg.num_all = 0;
     }
 
-    int exit_code = 0;
+    int result = 0;
 
-    if (i >= argc) {
-        int rc = process_stream(stdin, "-", &opts);
-        if (rc != 0) exit_code = rc;
-        return exit_code;
+    if (pos == argc) {
+        return copy_file(stdin, "-", &flg);
     }
 
-    for (; i < argc; ++i) {
-        const char *path = argv[i];
-        if (strcmp(path, "-") == 0) {
-            int rc = process_stream(stdin, "-", &opts);
-            if (rc != 0) exit_code = rc;
+    for (; pos < argc; pos++) {
+        FILE *f;
+
+        if (strcmp(argv[pos], "-") == 0) {
+            result |= copy_file(stdin, "-", &flg);
             continue;
         }
 
-        FILE *fp = fopen(path, "rb");
-        if (!fp) {
-            fprintf(stderr, "mycat: cannot open '%s': %s\n", path, strerror(errno));
-            exit_code = 1;
+        f = fopen(argv[pos], "rb");
+        if (!f) {
+            fprintf(stderr, "mycat: cannot open '%s': %s\n",
+                    argv[pos], strerror(errno));
+            result = 1;
             continue;
         }
 
-        int rc = process_stream(fp, path, &opts);
-        if (rc != 0) exit_code = rc;
-        fclose(fp);
+        result |= copy_file(f, argv[pos], &flg);
+        fclose(f);
     }
 
-    return exit_code;
+    return result;
 }
